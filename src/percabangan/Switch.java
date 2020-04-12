48c50c43c9d7ffe081f038c44af0c6deba22e651
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-12 13:03:21 +0700

    Switchjalan

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,21 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanjalan=3;
+        switch (jalanjalan){
+            case 1:
+                System.out.println("Anak Lapas");
+                break;
+            case 2:
+                System.out.println("Pacaran ja pang");
+                break;
+            case 3:
+                System.out.println("Jadi Obat Nyamuk");
+                break;
+            case 4:
+                System.out.println("Urang Halus");
+                break;
+        }
+    }
+}
